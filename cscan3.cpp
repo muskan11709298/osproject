907c5026eb303a3d0c1dfc6a703a7a11e2bd64de
muskan11709298 <48865539+muskan11709298@users.noreply.github.com>
2019-04-12 20:23:52 +0530

    Add files via upload

---
+++ cscan3.cpp
@@ -0,0 +1,15 @@
+class disk
+{
+private :
+    int request[101];
+int sorted_request[101];
+int number_of_request;
+int max;
+int direction;
+public :
+disk()
+{
+    cout<<"\nEnter maximum disk limit : ";
+    cin>>max;
+}
+}
